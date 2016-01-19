590d4a05d638af2b8ddaeb5b95077128e929958a
Josh Hodgson <josh.pbh@gmail.com>
2016-01-19 11:21:03 +0000

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,20 @@
+var gpio = require('rpi-gpio');
+
+gpio.setup(7, gpio.DIR_OUT, write);
+
+function write() {
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin 7');
+    });
+}
+
+
+function closePins() {
+    gpio.destroy(function() {
+        console.log('All pins unexported');
+    });
+}
+
+
+setTimeout(closePins, 2000);
---
+++ readme.md
@@ -0,0 +1,4 @@
+ATEM-Tally-Service
+------------------
+
+This is in the initial feasibility testing stage, it doesn't do any tally stuff.
