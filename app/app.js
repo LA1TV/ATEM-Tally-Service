3803712ac5fe9ce2c9a2a5d68c1d7b411f5ee5cf
Josh Hodgson <josh.pbh@gmail.com>
2016-01-19 14:58:53 +0000

    timer

--- app/app.js
+++ app/app.js
@@ -2,13 +2,6 @@ var gpio = require('rpi-gpio');
 
 gpio.setup(7, gpio.DIR_OUT, write);
 
-function write() {
-    gpio.write(7, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin 7');
-    });
-}
-
 
 function closePins() {
     gpio.destroy(function() {
@@ -16,6 +9,30 @@ function closePins() {
     });
 }
 
+function writeOn(pin){
+  gpio.write(pin, true, function(err) {
+      if (err) throw err;
+      console.log('Written on to pin '+pin);
+  });
+}
+function writeOff(pin){
+  gpio.write(pin, false, function(err) {
+      if (err) throw err;
+      console.log('Written off to pin '+pin);
+  });
+}
 
-setTimeout(function(){gpio.write(7, false, function(err){console.log("off!");});}, 20000);
-setTimeout(closePins, 30000);
+
+writeOn(7);
+setInterval(function(){
+  writeOff(7);
+  writeOn(11);
+  setTimeout(function(){
+    writeOff(11);
+    writeOn(12);
+    setTimeout(function(){
+      writeOff(12)
+      writeOn(7)
+    },10000)
+  }, 10000)
+  ,30000)
