acb13f19f6386f0ca3d2606cfe8cc86c4b906fb7
joshhodgson <josh.pbh@gmail.com>
2016-02-01 17:51:37 +0000

    first commit of object based model

--- app/app.js
+++ app/app.js
@@ -2,50 +2,33 @@ var gpio = require('onoff').Gpio;
 var ATEM = require('applest-atem');
 var env = require('node-env-file');
 env(__dirname + '/.env');
+
+var cameraID = process.env.cameraID.split(',');
+var cameraPins = process.env.cameraPins.split(',');
+
 console.log(process.env);
 
 var atem = new ATEM();
 atem.connect('192.168.72.51'); // Replace your ATEM switcher. address.
 
-var red1 = new gpio(4, 'out');
-var red2 = new gpio(8, 'out');
-var red3 = new gpio(9, 'out');
-
-var lastTallys;
-
-atem.on('stateChanged', function(err, state) {
-  if (lastTallys != state.tallys) {
-    console.log("New tally info! " + state.tallys);
-  }
-  lastTallys = state.tallys;
-
-  if (state.tallys[1] == 1) { //Camera 1
-    red1.write(1, function(err) {
-      if (err) throw err;
+function light(cameraID, programPin, friendlyName) {
+  this.cameraID = cameraID;
+  this.programPin = programPin;
+  this.friendlyName = friendlyName;
+  that = this;
+  this.led = new gpio(this.programPin, 'out');
+  this.init = function(atem) {
+    atem.on('stateChanged', function(err, state) {
+      if (state.tallys[that.cameraID] == 1) {
+        that.led.write(1, function(err) {
+          if (err) throw err;
+        });
+      } else {
+        that.led.write(0, function(err) {
+          if (err) throw err;
+        });
+      }
     });
-  } else {
-    red1.write(0, function(err) {
-      if (err) throw err;
-    });
-  }
-
-  if (state.tallys[2] == 1) { //Camera 2
-    red2.write(1, function(err) {
-      if (err) throw err;
-    });
-  } else {
-    red2.write(0, function(err) {
-      if (err) throw err;
-    });
-  }
-  if (state.tallys[3] == 1) { //Camera 3
-    red3.write(1, function(err) {
-      if (err) throw err;
-    });
-  } else {
-    red3.write(0, function(err) {
-      if (err) throw err;
-    });
-  }
-  //  console.log('finished callback');
-});
+  };
+}
+var red1 = new light(0, 4, 'Camera 1');
