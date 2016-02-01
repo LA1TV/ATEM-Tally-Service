4487615c1fb9e6d36833be0bc71c650bf791068b
Josh Hodgson <josh.pbh@gmail.com>
2016-02-01 09:40:51 +0000

    Fix bad reference

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 var gpio = require('onoff').Gpio;
 var ATEM = require('applest-atem');
+var env     = require('node-env-file');
+env(__dirname + '/.env');
+console.log(process.env);
 
 var atem = new ATEM();
 atem.connect('192.168.72.51'); // Replace your ATEM switcher. address.
@@ -9,22 +12,22 @@ var red2 = new gpio(8, 'out');
 var red3 = new gpio(9, 'out');
 
 atem.on('stateChanged', function(err, state) {
-  console.log(state.tally); // catch the ATEM tally state.
+  console.log(state.tallys); // catch the ATEM tally state.
 
-  if (state.tally[1] >= 2) { //Camera 1
+  if(state.tallys[1]>=2){ //Camera 1
     red1.writeSync(1);
-  } else {
+  }else {
     red1.writeSync(0);
   }
 
-  if (state.tally[2] >= 2) { //Camera 2
+  if(state.tallys[2]>=2){ //Camera 2
     red2.writeSync(1);
-  } else {
+  }else{
     red2.writeSync(0);
   }
-  if (state.tally[3] >= 2) { //Camera 3
+  if(state.tallys[3]>=2){ //Camera 3
     red3.writeSync(1);
-  } else {
+  }else{
     red3.writeSync(0);
   }
 });
