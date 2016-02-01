597585a237641a600b8a9da812dd53b97648584a
Josh Hodgson <josh.pbh@gmail.com>
2016-02-01 20:23:51 +0000

    fixed stupid error

--- app/app.js
+++ app/app.js
@@ -15,24 +15,27 @@ atem.connect(process.env.ATEMIP);
 var atemWatcher = new events.EventEmitter();
 var lastTallys = [];
 atem.on('stateChanged', function(err, state){
-  if (lastTallys == state.tallys)
-  {atemWatcher.emit('stateChanged', state.tallys);
-console.log('New tally info emitted');}
+  if (lastTallys != state.tallys) {
+    atemWatcher.emit('stateChanged', state.tallys);
+    console.log('New tally info emitted');
+    lastTallys = state.tallys;
+  }
 });
 
 
 function light(cameraID, programPin, friendlyName) {
+  console.log(friendlyName + ' created as new light.');
   this.cameraID = cameraID;
   this.programPin = programPin;
   this.friendlyName = friendlyName;
   this.led = new gpio(this.programPin, 'out');
   this.init = function(atem) {
     atemWatcher.on('stateChanged', function(err, state) {
-        this.led.write(state.tallys[this.cameraID] == 1, function(err) {
-          if (err) throw err;
-        });
+      this.led.write(state.tallys[this.cameraID] == 1, function(err) {
+        if (err) throw err;
+      });
     });
   }.bind(this);
 }
-var red1 = new light(0, 4, 'Camera 1');
+var red1 = new light(0, 14, 'Camera 1');
 red1.init(atem);
