cce980a40ec01fc113ef5058ba52d0ab6e15d82e
Josh Hodgson <josh.pbh@gmail.com>
2016-04-15 13:29:10 +0100

    Hint and add new logic for preview and inverse outputs

--- app/app.js
+++ app/app.js
@@ -1,45 +1,65 @@
-var gpio = require('onoff').Gpio;
+var Gpio = require('onoff').Gpio;
 var ATEM = require('applest-atem');
-var env = require('node-env-file');
 var events = require('events');
-env(__dirname + '/.env');
-
-var cameraIDs = process.env.CAMIDS.split(',');
-var cameraPins = process.env.CAMPINS.split(',');
+var config = require('./config.json');
+var lights = require('./lights.json');
 
 var atem = new ATEM();
-atem.connect(process.env.ATEMIP);
+atem.connect(config.atemIP);
 
 var atemWatcher = new events.EventEmitter();
 var lastTallys = [];
-atem.on('stateChanged', function(err, state){
-  if (lastTallys != state.tallys && state.tallys.length>1) {
+atem.on('stateChanged', function(err, state) {
+  if (lastTallys !== state.tallys && state.tallys.length > 1) {
     atemWatcher.emit('stateChanged');
     lastTallys = state.tallys;
   }
 });
 
-function light(cameraID, programPin, friendlyName) {
-  console.log(friendlyName + ' created as new light on pin ' + programPin + '.');
-  this.cameraID = cameraID;
-  this.programPin = programPin;
+function Light(inputId, pin, preview, invert, friendlyName) {
+  console.log(friendlyName + ' created as new light on pin ' + pin + '.');
+  this.inputId = inputId;
+  this.pin = pin;
   this.friendlyName = friendlyName;
-  this.led = new gpio(this.programPin, 'out');
+  this.invert = invert;
+  this.led = new Gpio(this.pin, 'out');
   var that = this;
   this.init = function(atem) {
     atemWatcher.on('stateChanged', function(err) {
-      that.led.write((atem.state.tallys[that.cameraID] == 1 || atem.state.tallys[that.cameraID] == 3)|0, function(err) {
-        if (err) throw err;
-      });
+      if (err) {
+        throw err;
+      }
+      var programState = atem.state.tallys[that.cameraID] === 1 || atem.state.tallys[that.cameraID] === 3;
+      var previewState = atem.state.tallys[that.cameraID] === 2 || atem.state.tallys[that.cameraID] === 3;
+      if (invert) {
+        programState = !programState;
+        previewState = !previewState;
+      }
+      if (preview) {
+        that.led.write(previewState, function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+      } else {
+        that.led.write(programState, function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+      }
     });
   };
 }
-atem.on('connect',function(){
+
+atem.once('connect', function() {
   var leds = [];
-  setTimeout(function(){
-    for (var i in cameraIDs){
-      leds[i]=new light(cameraIDs[i], cameraPins[i], atem.state.channels[+i+1].name);
-      leds[i].init(atem);
+  setTimeout(function() {
+    for (var i in lights) {
+      if (lights[i].inputID) {
+        leds[i] = new Light(lights[i].inputId, lights[i].pin, lights[i].preview, lights[i].invert, lights[i].name);
+        leds[i].init(atem);
+      }
     }
   }, 500);
 });
